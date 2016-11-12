9987badbee789ee6928a3fb02d72b22da46d0925
Jairo Arreola <eg6.2013@gmail.com>
2016-11-12 02:32:26 -0600

    Added the logic to just provide the link where all of the majors are shown and it will iterate through all of the majors and temporarily write all of the courses that are being offered that semester for each major to the /scrapedMajorsClasses folder, the webscraper currently creates a text file for each major and writes all of the courses for that major, for example the CPE major it will create a file named CPE.txt etc.

--- webScraping/JsoupLearningNetbeans/src/jsouplearningnetbeans/JsoupLearningNetbeans.java
+++ webScraping/JsoupLearningNetbeans/src/jsouplearningnetbeans/JsoupLearningNetbeans.java
@@ -5,6 +5,16 @@
  */
 package jsouplearningnetbeans;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  *
  * @author Jairo
@@ -16,6 +26,41 @@ public class JsoupLearningNetbeans {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String domainName = "http://www.uah.edu";
+        String url1 = domainName + "/cgi-bin/schedule.pl?file=sprg2017.html&segment=NDX";
+        Document doc;
+       
+        try {
+            
+            doc = Jsoup.connect(url1).get();
+
+            Elements majors = doc.select("pre");
+            Elements linkMajors = majors.select("a[href]");
+            
+            String descriptions[] = majors.first().ownText().split("\\r?\\n");
+            int descriptionsLength = descriptions.length;
+            int i = 0;
+
+            System.out.println("WebScraping.....Please wait");
+            for(Element link : linkMajors){
+                
+              if(i < descriptionsLength)
+              {
+                String majorUrl = domainName+ link.attr("href");
+                Document docMajor = Jsoup.connect(majorUrl).get();
+                Elements info = docMajor.select("pre");
+                BufferedWriter writer = new BufferedWriter(new FileWriter("scrapedMajorsClasses/" + link.ownText() + ".txt"));
+                writer.write(info.text());
+                writer.close();
+              }
+              i++;
+            }
+            
+            System.out.println("Finished..WebScraping...");
+        } catch (IOException ex) {
+            System.out.println("Error Getting URL");
+        }
+       
     }
     
 }
