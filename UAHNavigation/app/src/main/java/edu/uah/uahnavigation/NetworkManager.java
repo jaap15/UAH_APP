fdfeab003c55accef289f5ae528836514068efb3
Jairo Arreola <eg6.2013@gmail.com>
2017-01-06 02:43:02 -0600

    Added some comments to the NetworkManager class and added the jsoup library to the build gradle.

--- UAHNavigation/app/src/main/java/edu/uah/uahnavigation/NetworkManager.java
+++ UAHNavigation/app/src/main/java/edu/uah/uahnavigation/NetworkManager.java
@@ -9,10 +9,13 @@ import java.net.InetAddress;
 
 public class NetworkManager {
 
-
+    /**
+     * This static function is used to determine if there is an actual internet connection.
+     * @return true if the function was able to convert google.com to an ip address which means that there is internet connection otherwise false
+     */
     public static boolean hasInternetConnection() {
         try {
-            InetAddress ipAddr = InetAddress.getByName("google.com");
+            InetAddress ipAddr = InetAddress.getByName("google.com"); //Trying to get the ip address of google.com
             return !ipAddr.equals("");
 
         } catch (Exception e) {
