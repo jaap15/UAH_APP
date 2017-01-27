7c5cab892a19f07b5191a443e44d4291ebced8f7
Jairo Arreola <eg6.2013@gmail.com>
2017-01-26 19:17:13 -0600

    Removed Daniel's buttons and spinners from the Main Activity xml and commented out Daniel's unit tests that didn't run yet.

--- UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/DatabaseUnitTest.java
+++ UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/DatabaseUnitTest.java
@@ -5,6 +5,8 @@ import android.test.AndroidTestCase;
 
 import junit.framework.TestCase;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -19,26 +21,36 @@ import static org.mockito.Mockito.when;
  * Created by Daniel on 1/21/2017.
  */
 
-public class DatabaseUnitTest extends AndroidTestCase {
+public class DatabaseUnitTest{
 
-    @Mock
-    DatabaseManager databaseMock;
-
-    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-    @Test
-    public void databaseExistsThrowsException() throws Exception {
-        DatabaseSource test = Mockito.mock(DatabaseSource.class);
-        when(test.exists(this.getContext())).thenReturn(true);
-        assertEquals(test.exists(this.getContext()), true);
+    @Before
+    public void setUp() throws Exception
+    {
 
     }
-
-    @Test
-    public void databaseExistsTrue() throws Exception
+//    @Mock
+//    DatabaseManager databaseMock;
+//
+//    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+//
+//    @Test
+//    public void databaseExistsThrowsException() throws Exception {
+//        DatabaseSource test = Mockito.mock(DatabaseSource.class);
+//        when(test.exists(this.getContext())).thenReturn(true);
+//        assertEquals(test.exists(this.getContext()), true);
+//
+//    }
+//
+//    @Test
+//    public void databaseExistsTrue() throws Exception
+//    {
+//        DatabaseSource test = Mockito.mock(DatabaseSource.class);
+//        when(test.exists(this.getContext())).thenReturn(false);
+//        assertEquals(test.exists(this.getContext()), false);
+//    }
+    @After
+    public void tearDown() throws Exception
     {
-        DatabaseSource test = Mockito.mock(DatabaseSource.class);
-        when(test.exists(this.getContext())).thenReturn(false);
-        assertEquals(test.exists(this.getContext()), false);
+
     }
 }
