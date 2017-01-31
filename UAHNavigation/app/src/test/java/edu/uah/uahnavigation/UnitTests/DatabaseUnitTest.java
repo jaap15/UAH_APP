ee53ef2b3dde20142cf420535313c0440f81022e
Dburris13 <dab0021@uah.edu>
2017-01-30 19:48:12 -0600

    Git MERGE messed up my DatabaseUnitTest. This should fix it.

--- UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/DatabaseUnitTest.java
+++ UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/DatabaseUnitTest.java
@@ -1,40 +1,22 @@
 package edu.uah.uahnavigation.UnitTests;
 
-import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
-import android.test.AndroidTestCase;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import junit.framework.TestCase;
+import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import edu.uah.uahnavigation.DatabaseManager;
 import edu.uah.uahnavigation.DatabaseSource;
-import edu.uah.uahnavigation.NetworkManager;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.when;
-import android.support.test.runner.AndroidJUnit4;
-
-import android.support.test.*;
-import android.util.Log;
 
 /**
  * Created by Daniel on 1/21/2017.
@@ -51,7 +33,9 @@ public class DatabaseUnitTest {
 
     private DatabaseSource databaseSource;
 
+    public DatabaseUnitTest() {
 
+    }
 
     @Before
     public void setUp() {
@@ -59,28 +43,14 @@ public class DatabaseUnitTest {
         databaseSource = new DatabaseSource(aca);
         databaseSource.open();
     }
-//    @Mock
-//    DatabaseManager databaseMock;
-//
-//    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-//
-//    @Test
-//    public void databaseExistsThrowsException() throws Exception {
-//        DatabaseSource test = Mockito.mock(DatabaseSource.class);
-//        when(test.exists(this.getContext())).thenReturn(true);
-//        assertEquals(test.exists(this.getContext()), true);
-//
-//    }
-//
-//    @Test
-//    public void databaseExistsTrue() throws Exception
-//    {
-//        DatabaseSource test = Mockito.mock(DatabaseSource.class);
-//        when(test.exists(this.getContext())).thenReturn(false);
-//        assertEquals(test.exists(this.getContext()), false);
-//    }
+
     @After
-    public void tearDown() throws Exception
-    {
+    public void finish() {
+        databaseSource.close();
+    }
+
+    @Test
+    public void testPreConditions() {
+        assertNotNull(databaseSource);
     }
 }
