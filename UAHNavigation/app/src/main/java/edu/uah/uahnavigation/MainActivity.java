a2bf59fde2206c285cc73c4eb29ea66a220f6055
Jairo Arreola <eg6.2013@gmail.com>
2017-02-26 01:48:14 -0600

    Created a temporary sign keystore so we can also use the release build variant instead of just the debug build variant. I also thought that we will have a config file/properties file that will have the information like URL. We would need two of these config files one for testing (testing would have a file path instead of URL etc) and one for release (would have the actual url). I added a buildConfig field for determining which config/properties file will be used depending on the build type that is selected.

--- UAHNavigation/app/src/main/java/edu/uah/uahnavigation/MainActivity.java
+++ UAHNavigation/app/src/main/java/edu/uah/uahnavigation/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("myMessage", BuildConfig.CONFIG);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
