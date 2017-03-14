dc1b3afb2b386abfc9f2e9a93539a7c4912f3ed0
Dburris13 <dab0021@uah.edu>
2017-03-13 21:49:25 -0500

    Cleaned up build.gradle. All current tests still run. Added MyRunner custom Robolectric runner class. Refactored DatabaseUnitTest to be run MyRunner class.

--- UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/DatabaseUnitTest.java
+++ UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/DatabaseUnitTest.java
@@ -2,55 +2,34 @@ package edu.uah.uahnavigation.UnitTests;
 
 import android.app.Activity;
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
+import org.apache.tools.ant.Main;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.modules.testng.PowerMockTestCase;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import edu.uah.uahnavigation.DatabaseManager;
-import edu.uah.uahnavigation.DatabaseSource;
+import edu.uah.uahnavigation.BuildConfig;
+import edu.uah.uahnavigation.ClassTabActivity;
 import edu.uah.uahnavigation.MainActivity;
 
-import static junit.framework.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Daniel on 1/21/2017.
  */
-@Config(manifest="src/main/AndroidManifest.xml")
-@RunWith(RobolectricTestRunner.class)
+@RunWith(MyRunner.class)
+@Config(constants = BuildConfig.class, sdk = 18)
 public class DatabaseUnitTest  {
 
-
-    private MainActivity activity;
-    Context c = new Activity();
-
-    @Before
-    public void setup() {
-        activity = Robolectric.buildActivity(MainActivity.class).create().get();
-    }
-
     @Test
-    public void checkActivityNotNull() throws Exception {
-        assertNotNull(activity);
+    public void testSomething() throws Exception {
+        assertTrue(Robolectric.setupActivity(MainActivity.class) != null);
     }
 }
+
 
 
 
---
+++ UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/MyRunner.java
@@ -0,0 +1,92 @@
+package edu.uah.uahnavigation.UnitTests;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.res.FileFsFile;
+import org.robolectric.util.Logger;
+import org.robolectric.util.ReflectionHelpers;
+
+/**
+ * Created by Daniel on 3/13/2017.
+ */
+
+public class MyRunner extends RobolectricTestRunner {
+
+    private static final String BUILD_OUTPUT = "app/build/intermediates";
+
+    public MyRunner(Class<?> klass) throws InitializationError {
+        super(klass);
+    }
+
+    @Override
+    protected AndroidManifest getAppManifest(Config config) {
+        if (config.constants() == Void.class) {
+            Logger.error("Field 'constants' not specified in @Config annotation");
+            Logger.error("This is required when using RobolectricGradleTestRunner!");
+            throw new RuntimeException("No 'constants' field in @Config annotation!");
+        }
+
+        final String type = getType(config);
+        final String flavor = getFlavor(config);
+        final String packageName = getPackageName(config);
+
+        final FileFsFile res;
+        final FileFsFile assets;
+        final FileFsFile manifest;
+
+        if (FileFsFile.from(BUILD_OUTPUT, "res").exists()) {
+            res = FileFsFile.from(BUILD_OUTPUT, "res", flavor, type);
+        } else {
+            res = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "res");
+        }
+
+        if (FileFsFile.from(BUILD_OUTPUT, "assets").exists()) {
+            assets = FileFsFile.from(BUILD_OUTPUT, "assets", flavor, type);
+        } else {
+            assets = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "assets");
+        }
+
+        if (FileFsFile.from(BUILD_OUTPUT, "manifests").exists()) {
+            manifest = FileFsFile.from(BUILD_OUTPUT, "manifests", "full", flavor, type, "AndroidManifest.xml");
+        } else {
+            manifest = FileFsFile.from(BUILD_OUTPUT, "bundles", flavor, type, "AndroidManifest.xml");
+        }
+
+        Logger.debug("Robolectric assets directory: " + assets.getPath());
+        Logger.debug("   Robolectric res directory: " + res.getPath());
+        Logger.debug("   Robolectric manifest path: " + manifest.getPath());
+        Logger.debug("    Robolectric package name: " + packageName);
+        return new AndroidManifest(manifest, res, assets, packageName);
+    }
+
+    private String getType(Config config) {
+        try {
+            return ReflectionHelpers.getStaticField(config.constants(), "BUILD_TYPE");
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    private String getFlavor(Config config) {
+        try {
+            return ReflectionHelpers.getStaticField(config.constants(), "FLAVOR");
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    private String getPackageName(Config config) {
+        try {
+            final String packageName = config.packageName();
+            if (packageName != null && !packageName.isEmpty()) {
+                return packageName;
+            } else {
+                return ReflectionHelpers.getStaticField(config.constants(), "APPLICATION_ID");
+            }
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+}
