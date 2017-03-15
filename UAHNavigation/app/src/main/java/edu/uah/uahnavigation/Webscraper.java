0628ca0a695e6c1504c0ba3855f16513cc77d556
Jairo Arreola <eg6.2013@gmail.com>
2017-03-15 14:02:23 -0500

    Added the scraping functionality for all of the majors and added a static class that will be a Semester object that will hold the semester name and the url to that semester.

--- UAHNavigation/app/src/main/java/edu/uah/uahnavigation/Webscraper.java
+++ UAHNavigation/app/src/main/java/edu/uah/uahnavigation/Webscraper.java
@@ -1,40 +1,183 @@
 package edu.uah.uahnavigation;
 
+import android.content.Context;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 /**
  * Created by Jairo on 12/22/2016.
  */
 
 public class Webscraper {
 
-    private String homePageUrl = null;
+    private Context context;
+    private String domainName = null;
+    private String courseListingURL = null;
     private String semesterUrlToScrape = null;
+    private Semester semesterToScrape;
+    private ArrayList<Semester> currentSemesters;
+    private String webscrapeResourcesFolderPath;
 
     public Webscraper()
     {
-        this.homePageUrl = "http://www.uah.edu/cgi-bin/schedule.pl";
+//        this.homePageUrl = "http://www.uah.edu/cgi-bin/schedule.pl";
     }
 
-    public Webscraper(String homePageUrl)
+    public Webscraper(Context context, String domainName, String courseListingURL, String webscrapeResourcesFolder)
     {
-        this.homePageUrl = homePageUrl;
-    }
+        this.context = context;
+        this.domainName = domainName;
+        this.courseListingURL = courseListingURL;
+        this.semesterToScrape = new Semester();
+        this.webscrapeResourcesFolderPath = context.getFilesDir() + "/" + webscrapeResourcesFolder;
+        File direct = new File(this.webscrapeResourcesFolderPath);
 
-    public String getHomePageUrl()
-    {
-        return this.homePageUrl;
-    }
-
-    public void setSemesterUrlToScrape(String semesterUrlToScrape)
-    {
-        if(semesterUrlToScrape.startsWith("http") || semesterUrlToScrape.startsWith("www."))
-        {
-            this.semesterUrlToScrape = semesterUrlToScrape;
+        if(!direct.exists()) {
+            if(direct.mkdir()); //directory is created;
         }
+
+        this.currentSemesters = new ArrayList<Semester>();
+    }
+
+    public void scrapePossibleSemesters()
+    {
+        this.currentSemesters.add(new Semester("URL1", "Spring"));
+        this.currentSemesters.add(new Semester("URL2", "Summer"));
+        this.currentSemesters.add(new Semester("URL3", "Fall"));
+    }
+
+    public ArrayList<Semester> getPossibleSemesters()
+    {
+        return this.currentSemesters;
+    }
+
+    public void setSemesterToScrape(Semester semesterToScrape)
+    {
+        if(semesterToScrape.getURL().startsWith("http") || semesterToScrape.getURL().startsWith("www."))
+        {
+            this.semesterToScrape.setURL(semesterToScrape.getURL());
+            this.semesterToScrape.setSemesterName(semesterToScrape.getSemesterName());
+        }
+        else
+        {
+            Log.d("myMessage", "Invalid URL to Scrape!!\n " + semesterToScrape.getURL());
+            throw new RuntimeException("Invalid URL to Scrape!!\n " + semesterToScrape.getURL());
+        }
+
+    }
+
+    public void scrapeSemester()
+    {
+        Document doc;
+        ArrayList courseList = new ArrayList();
+        createSemesterDirectory();
+
+        try {
+
+//            String url1 = domainName + courseListingURL;
+            String url1 = this.semesterToScrape.getURL();
+            String FOLDER_NAME = this.webscrapeResourcesFolderPath + "/" + this.semesterToScrape.getSemesterName();
+            doc = Jsoup.connect(url1).get();
+            Elements majors = doc.select("pre");
+            Elements linkMajors = majors.select("a[href]");
+
+            String descriptions[] = majors.first().ownText().split("\\r?\\n");
+            int descriptionsLength = descriptions.length;
+            int i = 0;
+
+            System.out.println("WebScraping.....Please wait");
+            for(Element link : linkMajors){
+
+                if(i < descriptionsLength)
+                {
+                    String majorUrl = domainName+ link.attr("href");
+                    Document docMajor = Jsoup.connect(majorUrl).get();
+                    Elements info = docMajor.select("pre");
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(FOLDER_NAME + "/" + link.ownText() + ".txt"));
+                    courseList.add(FOLDER_NAME + "/" + link.ownText() + ".txt");
+                    writer.write(info.text());
+                    writer.close();
+                }
+                i++;
+            }
+
+            System.out.println("Finished..WebScraping...");
+        } catch (Exception ex) {
+            System.out.println("Error Getting URL");
+        }
+    }
+
+    private void createSemesterDirectory()
+    {
+        File file = new File(this.webscrapeResourcesFolderPath + "/" + this.semesterToScrape.getSemesterName());
+        boolean b = false;
+
+        if(!file.exists())
+        {
+            System.out.println("making directory");
+            b = file.mkdir();
+
+            if(b)
+            {
+                System.out.println("Directory successfully created");
+            }
+            else
+            {
+                System.out.println("Unable to create directory");
+            }
+        }
+        else
+        {
+            System.out.println("directory already exists");
+        }
+
+
     }
 
     public String getSemesterUrlToScrape()
     {
         return this.semesterUrlToScrape;
     }
+//    public String getHomePageUrl() { return this.homePageUrl; }
 
+    static class Semester{
+        private String URL = null;
+        private String semesterName = null;
+
+        public Semester()
+        {
+
+        }
+
+        public Semester(String URL, String semesterName)
+        {
+            this.URL = URL;
+            this.semesterName = semesterName;
+        }
+
+        public String getURL() {
+            return URL;
+        }
+
+        public void setURL(String URL) {
+            this.URL = URL;
+        }
+
+        public String getSemesterName() {
+            return semesterName;
+        }
+
+        public void setSemesterName(String semesterName) {
+            this.semesterName = semesterName;
+        }
+    }
 }
--- UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/WebscraperUnitTest.java
+++ UAHNavigation/app/src/test/java/edu/uah/uahnavigation/UnitTests/WebscraperUnitTest.java
@@ -24,44 +24,44 @@ public class WebscraperUnitTest {
     {
         String url = "http://www.uah.edu/cgi-bin/schedule.pl";
         scraper = new Webscraper();
-        assertEquals(url, scraper.getHomePageUrl());
+//        assertEquals(url, scraper.getHomePageUrl());
 
     }
 
-    @Test
-    public void testWebscraperConstructorWithParameter()
-    {
-        String url = "http://www.Arreola.com";
-        scraper = new Webscraper(url);
-        assertEquals(url, scraper.getHomePageUrl());
-
-    }
-
-    @Test
-    public void testWebscraperSetAndGetSemesterUrlToScrape()
-    {
-        String url = "";
-        scraper = new Webscraper();
-        scraper.setSemesterUrlToScrape(url);
-        assertEquals(null, scraper.getSemesterUrlToScrape());
-
-        url = "123456";
-        scraper.setSemesterUrlToScrape(url);
-        assertEquals(null, scraper.getSemesterUrlToScrape());
-
-        url = "Jairo.com";
-        scraper.setSemesterUrlToScrape(url);
-        assertEquals(null, scraper.getSemesterUrlToScrape());
-
-        url = "http://www.Jairo.com";
-        scraper.setSemesterUrlToScrape(url);
-        assertEquals(url, scraper.getSemesterUrlToScrape());
-
-        url = "www.Jairo.com";
-        scraper.setSemesterUrlToScrape(url);
-        assertEquals(url, scraper.getSemesterUrlToScrape());
-
-    }
+//    @Test
+//    public void testWebscraperConstructorWithParameter()
+//    {
+//        String url = "http://www.Arreola.com";
+//        scraper = new Webscraper(url);
+//        assertEquals(url, scraper.getHomePageUrl());
+//
+//    }
+//
+//    @Test
+//    public void testWebscraperSetAndGetSemesterUrlToScrape()
+//    {
+//        String url = "";
+//        scraper = new Webscraper();
+//        scraper.setSemesterUrlToScrape(url);
+//        assertEquals(null, scraper.getSemesterUrlToScrape());
+//
+//        url = "123456";
+//        scraper.setSemesterUrlToScrape(url);
+//        assertEquals(null, scraper.getSemesterUrlToScrape());
+//
+//        url = "Jairo.com";
+//        scraper.setSemesterUrlToScrape(url);
+//        assertEquals(null, scraper.getSemesterUrlToScrape());
+//
+//        url = "http://www.Jairo.com";
+//        scraper.setSemesterUrlToScrape(url);
+//        assertEquals(url, scraper.getSemesterUrlToScrape());
+//
+//        url = "www.Jairo.com";
+//        scraper.setSemesterUrlToScrape(url);
+//        assertEquals(url, scraper.getSemesterUrlToScrape());
+//
+//    }
 
     @After
     public void tearDown() throws Exception {
