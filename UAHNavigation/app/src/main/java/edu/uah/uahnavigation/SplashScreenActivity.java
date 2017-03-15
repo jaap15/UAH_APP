0cfc3aad214e8f48b961549cc3be2a4787161ea0
Komlan Maglo <kmm0048@uah.edu>
2017-03-15 13:06:40 -0500

    Simple Splash screen implementation

--- UAHNavigation/app/src/main/java/edu/uah/uahnavigation/SplashScreenActivity.java
+++ UAHNavigation/app/src/main/java/edu/uah/uahnavigation/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package edu.uah.uahnavigation;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 4000);
     }
 }
