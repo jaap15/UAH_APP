003df405be4ecea14a7803f8c299bbdaada5277e
Adam Walters <atw0015@uah.edu>
2017-03-17 14:18:17 -0500

    Added Graph file for MSB. Added Parsing of Graph file to InteriorNavigationActivity.

--- UAHNavigation/app/src/main/java/edu/uah/uahnavigation/InteriorNavigationActivity.java
+++ UAHNavigation/app/src/main/java/edu/uah/uahnavigation/InteriorNavigationActivity.java
@@ -1,10 +1,24 @@
 package edu.uah.uahnavigation;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class InteriorNavigationActivity extends AppCompatActivity {
 
@@ -12,6 +26,8 @@ public class InteriorNavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interior_navigation);
+        readInputFile();
+
     }
 
     public void didTapGreetButton(View view) {
@@ -26,5 +42,70 @@ public class InteriorNavigationActivity extends AppCompatActivity {
 
         messageTextView.setText(greeting);
 
+    }
+
+    private void readInputFile() {
+        int START_LINE = 2;
+        AssetManager mngr = getAssets();
+        try{
+            InputStream in = mngr.open("InteriorNavigationResources/ENG/Graph.txt");   //Need to generalize this path
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder out = new StringBuilder();
+            String line = "";
+            String SourceNode = "";
+            int TotalSources = 0;
+            String DestinationNode = "";
+            String WeightStr = "";
+            int Weight = 0;
+            String FloorPlan = "";
+            int NumEdges = 0;
+            int TotalEdges = 0;
+            for(int j = 0; j < START_LINE; j++) {                   //Skips First Two Lines in .txt
+                reader.readLine();
+            }
+
+            String[] token;
+            //Log.d("aMessage", "Token length: " + token.length);
+
+            int i = 3;
+            int k = 0;
+            //Log.d("aMessage", "Test");
+
+            while ((line = reader.readLine()) != null) {
+                    token = line.split(" ");
+                //Log.d("aMessage", "Test: " + line.substring(0,1).compareTo("@"));
+                //Log.d("aMessage", "First: " + line.substring(0,1));
+                if(line.substring(0,1).compareTo("#") == 0){
+                    token[0] = line.substring(1) + ".PNG";          //Append .PNG to grab Floor Plan
+                    FloorPlan = token[0];
+                    Log.d("aMessage", "Case1 " + "Line " + i +":"+ "FloorPlan: " + FloorPlan);          //Case 1: Floor. Need to open floorplan
+                }
+                else if(line.substring(0,1).compareTo("@") == 0){
+                    token[0] = line.substring(1);
+                    NumEdges = Integer.parseInt(line.substring(1));
+                    TotalEdges = TotalEdges + NumEdges;
+                    TotalSources = TotalSources + 1;
+                    Log.d("aMessage","Case2 " + "Line " + i +":"+ "NumEdges: " + NumEdges);          //Case 2: Edge. Defines EdgeNum for next vertex
+                }
+                else {
+                    SourceNode = token[0];
+                    Log.d("aMessage","Case3 " + "Line " + i + ":" + "SourceNode: " + SourceNode);
+                    DestinationNode = token[1];
+                    Log.d("aMessage", "Case3 " + "Line " + i + ":" + "DestinationNode: " + DestinationNode);
+                    WeightStr = token[2];
+                    Weight = Integer.parseInt(WeightStr);
+                    Log.d("aMessage", "Case3 " + "Line " + i + ":" + "Weight: " + Weight);
+                }
+                i++;
+
+            } //End while
+
+            Log.d("aMessage","Total Edges: " + TotalEdges);
+            Log.d("aMessage","Total Source Nodes: " + TotalSources);
+
+        } catch(IOException e) {
+            Log.d("aMessage", "Exception");
+            e.printStackTrace();
+        }
     }
 }
