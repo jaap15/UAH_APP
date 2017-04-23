156e50d23b24bf18eb7ac32e692173c6e5bea8cf
Dburris13 <dab0021@uah.edu>
2017-04-22 20:55:33 -0500

    Something went wrong with old API key. Generated a new one and got external route planning working again.

--- UAHNavigation/app/src/main/java/edu/uah/modules/DirectionFinder.java
+++ UAHNavigation/app/src/main/java/edu/uah/modules/DirectionFinder.java
@@ -28,7 +28,7 @@ import edu.uah.uahnavigation.DialogException;
  */
 public class DirectionFinder {
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
-    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    private static final String GOOGLE_API_KEY = "AIzaSyD2Iv8zfpCI5pTNhJI78GuTcEoTbGiuGCo";
     private DirectionFinderListener listener;
     private String origin;
     private String destination;
